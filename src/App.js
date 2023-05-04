b5fa4ab932e9b59e463c0962c0030feb91b54a65
Chase <chasedemaster.tech@gmail.com>
2023-05-04 11:29:33 -0500

    add nav component +

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import About from './components/About/index';
+import Nav from './components/Nav/index'
 
 function App() {
   return (
     <div className="App">
+      <Nav />
+      <main>
       <About />
+      </main>
     </div>
   );
 }
