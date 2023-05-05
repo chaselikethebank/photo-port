a0bf1d8b040590a488421cdc00799ff29c7f5d86
Chase <chasedemaster.tech@gmail.com>
2023-05-05 14:42:35 -0500

    timer component and some work on gallery

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import About from './components/About/index';
 import Nav from './components/Nav/index'
+import Gallery from "./components/Gallery";
+
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <main>
+      <Gallery />
       <About />
       </main>
     </div>
