ed3b946379b5eef25a68c07c535ed422446e849d
Chase <chasedemaster.tech@gmail.com>
2023-05-09 10:25:44 -0500

    conditionally render contact form

--- src/App.js
+++ src/App.js
@@ -1,37 +1,47 @@
 // import './App.css';
-import React, { useState } from 'react';
-import About from './components/About/index';
-import Nav from './components/Nav/index'
+import React, { useState } from "react";
+import About from "./components/About/index";
+import Nav from "./components/Nav/index";
 import Gallery from "./components/Gallery";
-import ContactForm from "./components/Contact/index"
-
+import ContactForm from "./components/Contact/index";
 
 function App() {
-  
+  const [contactSelected, setContactSelected] = useState(false);
+
   const [categories] = useState([
     {
-      name: 'commercial',
-      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
     },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
     <div className="App">
-      <Nav 
+      <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
-      />
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
       <main>
-      <ContactForm />
-
-     <Gallery currentCategory={currentCategory} />
-      <About />
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
